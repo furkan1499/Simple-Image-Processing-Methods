ecce36f5b17d8699b9a6ff09725b5a8738fe4db8
furkan1499 <furkan14991499@gmail.com>
2021-09-24 13:49:08 +0300

    Add Sobel Operator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 
+print("---start blending images---")
 # take two images
 img1 = cv2.imread('heisenberg.jpg')
 img2 = cv2.imread('heisenberg2.jpg')
@@ -13,7 +14,22 @@ img1 = cv2.resize(img1, (600, 300))
 img2 = cv2.resize(img2, (600, 300))
 
 # we give the mixing ratios of the images
-blended = cv2.addWeighted(src1=img1, alpha=0.85, src2=img2, beta=0.15, gamma=0)
+blended = cv2.addWeighted(src1=img1, alpha=0.8, src2=img2, beta=0.2, gamma=0)
 
 # we write image
 cv2.imwrite('blending.jpg', blended)
+
+print("---end blending images---")
+
+print("---start sobel operator---")
+# Sobel Operator
+# You can search Sobel Operator on Google. https://en.wikipedia.org/wiki/Sobel_operator
+
+img = cv2.imread('heisenberg.jpg', 0)
+sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+sobely = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+
+cv2.imwrite("sobelx_heisenberg.jpg", sobelx)
+cv2.imwrite("sobely_heisenberg.jpg", sobely)
+
+print("---end sobel operator---")
